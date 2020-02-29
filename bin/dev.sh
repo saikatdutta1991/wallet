89d9f6ac2d15f8e34b7ab6bfe53b9a7125825d87
Saikat Dutta <saikatdutta@Saikats-MacBook-Air.local>
2020-02-29 21:23:10 +0530

    .env config & scripts done

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,3 +1,22 @@
-echo "Starting developement environment";
+echo "Starting developement environment ..";
 
-DOTENV_CONFIG_PATH=$(pwd)/.env.development nodemon -r dotenv/config src/main/index.js
+envFile=".env";
+devEnvFile=".env.development";
+ 
+echo 'Removing old .env file ..';
+if [ -f "$envFile" ]
+then
+    rm "$envFile"
+fi
+
+echo 'Copy contents from .env.development > .env ..';
+if [ -f "$devEnvFile" ]
+then
+    cp "$devEnvFile" "$envFile"
+else
+    echo "$devEnvFile could not be found so exiting..";
+    exit 1;
+fi
+
+echo 'Starting node src/main/index.js'
+DOTENV_CONFIG_PATH=$(pwd)/.env nodemon -r dotenv/config src/main/index.js
--- bin/prod.sh
+++ bin/prod.sh
@@ -1,3 +1,22 @@
-echo "Starting production environment";
+echo "Starting production environment ..";
 
-DOTENV_CONFIG_PATH=$(pwd)/.env.production node -r dotenv/config src/main/index.js
+envFile=".env";
+prodEnvFile=".env.production";
+ 
+echo 'Removing old .env file ..';
+if [ -f "$envFile" ]
+then
+    rm "$envFile"
+fi
+
+echo 'Copy contents from .env.development > .env ..';
+if [ -f "$prodEnvFile" ]
+then
+    cp "$prodEnvFile" "$envFile"
+else
+    echo "$prodEnvFile could not be found so exiting..";
+    exit 1;
+fi
+
+echo 'Starting node src/main/index.js'
+DOTENV_CONFIG_PATH=$(pwd)/.env node -r dotenv/config src/main/index.js
